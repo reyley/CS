391517d99fe0f5b8f4587246b4ae87702fe34646
reyley <reyley1014@gmail.com>
2016-04-03 23:43:18 -0700

    project 4 nand2tetris

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -8,4 +8,75 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+
+
+    @KBD
+    D=M
+    @prev
+    M=D
+    @FILLSCREEN // if keyboard not zero: fill screen
+    D;JNE
+    @CHECKKEYS  // else continue checking the keyboard
+    0;JEQ
+
+(CHECKKEYS)
+    @KBD
+    D=M
+
+    @FILLSCREEN // if keyboard not zero: fill screen
+    D;JNE
+
+    @prev
+    D=D|M
+
+    @CHECKKEYS // loop back if prev and now are both 0
+    D;JEQ
+
+    @EMPTYSCREEN  // else empty screen (now is 0 and prev was 1)
+    D;JNE
+
+(FILLSCREEN)
+    @screenstat
+    M=-1
+    @prev
+    M=1
+    @CHANGESCREEN
+    0;JEQ
+
+(EMPTYSCREEN)
+    @screenstat
+    M=0
+    @prev
+    M=0
+    @CHANGESCREEN
+    0;JEQ
+
+(CHANGESCREEN)
+    @SCREEN  // set location to point to the start of the screen
+    D=A
+    @location
+    M=D
+
+    @LOOP
+    0;JEQ
+
+(LOOP)
+// colour row location in the colour of screenstat
+    @screenstat
+    D=M
+    @location
+    A=M
+    M=D
+
+// add 1 to location to get the next location
+    @location
+    MD=M+1
+// checking if our next location is out of the screen and into the keyboard location
+    @KBD
+    D=A-D
+
+    @CHECKKEYS  // if location + 1 => KBRD location: go back to checking the keyboard
+    D;JEQ
+
+    @LOOP  // if location + 1 is in the screen loop over
+    D;JGT
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -7,3 +7,113 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// a = RAM[0], b = Ram[1]
+    @R0
+    D=M
+    @a
+    M=D
+
+    @R1
+    D=M
+    @b
+    M=D
+
+// if b<0 flip a and b
+    @b
+    D=M
+    @FLIP
+    D;JLT
+    @STARTMUL
+    0;JEQ
+
+
+
+(FLIP)
+    @a
+    D=M
+    @temp
+    M=D  //temp=a
+
+    @b
+    D=M
+    @a
+    M=D  //a=b
+
+    @temp
+    D=M
+    @b
+    M=D  //b=a
+
+    @FLIPPED
+    0;JEQ
+
+
+(FLIPPED)
+// if b<0 again multiply a and b by -1
+    @b
+    D=M
+    @CHANGESIGN
+    D;JLT
+    @STARTMUL
+    0;JEQ
+
+(CHANGESIGN)
+    @a
+    M=-M
+    @b
+    M=-M
+    @STARTMUL
+    0;JEQ
+
+(STARTMUL)
+// i=0
+    @i
+    M=0
+
+// result=0
+    @result
+    M=0
+
+// if i < b goto loop
+    @b
+    D=M
+    @i
+    D=M-D  // D = i-b
+    @LOOP
+    D;JLT
+    @SETRESULT
+    0;JEQ
+
+(LOOP)
+    @a
+    D=M
+    @result
+    M=M+D  // result += a
+
+    @i
+    M=M+1 // i++
+
+// if i < b goto loop
+    @b
+    D=M
+    @i
+    D=M-D  // D = i-b
+    @LOOP
+    D;JLT
+
+// if i = b: R2 = result, END
+    @SETRESULT
+    0;JEQ
+
+(SETRESULT)
+    @result
+    D=M
+    @R2
+    M=D
+    @END
+    0;JEQ
+
+(END)
+    @END
+    0;JEQ
